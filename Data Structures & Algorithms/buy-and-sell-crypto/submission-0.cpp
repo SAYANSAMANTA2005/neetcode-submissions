dd365a114ae82c62203c474e17d865ddb798d260
Sayan Samanta <sayansamantabirth2005@gmail.com>
2026-06-01 04:39:42 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0,mx=0;
+        int l=0,r=prices.size()-1,n=prices.size();
+       for(int i=n-1;i>=0;i--){
+        profit=max(profit,mx-prices[i]);
+        mx=max(mx,prices[i]);
+       }
+       return profit;
+    }
+};
