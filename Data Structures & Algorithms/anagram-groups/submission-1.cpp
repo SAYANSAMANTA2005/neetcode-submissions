0aca1edd9e6fb3b606dae8d37112dc4532e8d9d6
Sayan Samanta <sayansamantabirth2005@gmail.com>
2026-05-31 17:04:09 +0530

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for(auto it:strs){
+            auto i=it;
+            sort(it.begin(),it.end());
+            mp[it].push_back(i);
+        }
+        vector<vector<string>>v;
+        for(auto p:mp){
+            v.push_back(p.second);
+        }
+        return v;
+    }
+};
