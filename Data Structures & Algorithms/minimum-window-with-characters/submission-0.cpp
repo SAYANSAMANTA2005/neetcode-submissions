cc4139234815618ad632802350be88facf11ef8c
Sayan Samanta <sayansamantabirth2005@gmail.com>
2026-06-05 03:34:20 +0530

    Add: minimum-window-with-characters - submission-0

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int l=0,r=0,mnlen=1e9,start=-1;
+         vector<int>freq(100,0),need(100,0);
+          int required=0,have=0;
+         for(auto x:t){need[x-'A']++;
+         required+=need[x-'A']==1;}
+        
+        
+        while(r<s.size()){
+           freq[s[r]-'A']++;
+           if(freq[s[r]-'A']==need[s[r]-'A'])have++;
+         while(have==required){
+
+            mnlen=min(mnlen,r-l+1);
+            if(mnlen==r-l+1)start=l;
+             freq[s[l]-'A']--;
+             if(freq[s[l]-'A']<need[s[l]-'A'] && need[s[l]-'A'])have--;
+            l++;
+         }
+           
+           r++;
+        }
+         if(start==-1)return "";
+        // cout<<(int)'A'<<" "<<(int)'a'<<endl;
+        return s.substr(start,mnlen);
+    }
+};
