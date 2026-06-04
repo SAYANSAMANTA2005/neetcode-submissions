07716e014b48808f4f6f482905b76ec9ac820781
Sayan Samanta <sayansamantabirth2005@gmail.com>
2026-06-04 14:16:33 +0530

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    set<vector<int>>v;
+    void solve(vector<int>&nums,int i,int target,vector<int>&curr){
+      if(i==nums.size()){
+        if(target==0)
+        v.insert(curr);
+        return;
+      }
+      //skip ith element
+      solve(nums,i+1,target,curr);
+      //use ith element
+      if(target>=nums[i]){
+      curr.push_back(nums[i]);
+      solve(nums,i,target-nums[i],curr);
+      //backtracking
+      curr.pop_back();
+      }
+
+
+    }
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        sort(nums.begin(),nums.end());
+        vector<int>curr;
+        solve(nums,0,target,curr);
+        vector<vector<int>>ans(v.begin(),v.end());
+        return ans;
+    }
+};
