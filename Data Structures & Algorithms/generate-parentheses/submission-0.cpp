8bd5582d52d6385bc080b104603d30aab80e4fe4
Sayan Samanta <sayansamantabirth2005@gmail.com>
2026-06-08 03:07:02 +0530

    Add: generate-parentheses - submission-0

---
+++ Data Structures & Algorithms/generate-parentheses/submission-0.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+vector<string>ans;
+   void solve(string s,int i,int n,int opclose){
+    if(i==2*n){
+    if(opclose==0){
+     ans.push_back(s);
+     }
+     return;
+    }//
+    if(opclose<0){
+       // s="";
+        return;}
+
+    //op1-- (
+    s+='(';
+    solve(s,i+1,n,opclose+1);
+    s.pop_back();
+    //op2 -- ')
+    s+=')';
+    solve(s,i+1,n,opclose-1);
+
+   }
+    vector<string> generateParenthesis(int n) {
+        string s;
+        solve(s,0,n,0);
+        return ans;
+    }
+};
