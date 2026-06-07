3ae8622952191794017870803e1c9f50ffbde379
Sayan Samanta <sayansamantabirth2005@gmail.com>
2026-06-08 03:58:08 +0530

    Add: palindrome-partitioning - submission-3

---
+++ Data Structures & Algorithms/palindrome-partitioning/submission-3.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+bool check(vector<string>&ans){
+    for(auto &x:ans){
+        int l=0,r=x.size()-1;
+        while(l<r){
+            if(x[l]!=x[r]){
+                return 0;
+                
+            }
+            l++;r--;
+        }
+    }
+    return 1;
+}
+  vector<vector<string>>res;
+  void solve(int i,vector<string>&ans,string &s,string &curr){
+    if(i==s.size()){
+        if(check(ans))
+        res.push_back(ans);
+    return;
+    }//
+
+    //op1 --> include that in curr string
+    string it;
+    if(ans.size()){
+    it=curr;
+    ans.pop_back();
+    }
+    it+=s[i];
+    ans.push_back(it);
+    curr+=s[i];
+    solve(i+1,ans,s,curr);
+     if(ans.size()){
+        ans.back().pop_back();
+     }
+     curr.pop_back();
+
+
+    //op2 -- create a new string
+    if(ans.size() &&ans.back().size()!=0){
+    it="";it+=s[i];
+    ans.push_back(it);
+    solve(i+1,ans,s,it);
+    ans.pop_back();
+    }
+
+    
+
+
+  }
+  
+    vector<vector<string>> partition(string s) {
+        vector<string>ans;
+        string curr;
+        //cout<<ans.back();
+         solve(0,ans,s,curr);
+        return res;
+    }
+};
