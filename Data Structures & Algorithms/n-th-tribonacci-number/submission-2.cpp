c10698b9ccdd0ffa27215a1155c3a14cdb7241a1
Sayan Samanta <sayansamantabirth2005@gmail.com>
2026-07-03 20:04:55 +0530

    Add: n-th-tribonacci-number - submission-2

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-2.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        vector<int>tri(n+1,0);
+        if(n==0)return 0;
+        if(n<=2)return 1;
+        tri[1]=tri[2]=1;
+        for(int i=3;i<=n;i++)tri[i]=tri[i-1]+tri[i-2]+tri[i-3];
+        return tri[n];
+    }
+};
